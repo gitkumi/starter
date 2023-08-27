4931554d9093c0a7d976da91125f0509408dd399
Takumi <ito.tkmi@gmail.com>
2023-08-28 02:25:56 +0900

    Update vite config

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import App from './App'
-import './main.css'
+import './index.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,31 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import { resolve, join } from 'node:path'
+import { globSync } from 'glob'
+
+const ASSETS_DIR = '_bundle'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    assetsDir: ASSETS_DIR,
+    emptyOutDir: true,
+    outDir: join(__dirname, "dist"),
+    rollupOptions: {
+      input: globSync(resolve(__dirname, "src", "*.html")),
+      output: {
+        assetFileNames: (_assetInfo) => {
+          return `${ASSETS_DIR}/[name]-[hash][extname]`
+        }
+      }
+    },
+  },
   plugins: [react()],
+  publicDir: resolve(__dirname, 'public'),
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src')
+    }
+  },
+  root: 'src',
 })
