b9aeff730eb52e86346b0aca231a822751440674
Takumi Ito <ito.tkmi@gmail.com>
2024-10-02 05:10:23 +0900

    MPA vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,35 @@
+import path from 'path'
+import { fileURLToPath } from 'url'
 import react from '@vitejs/plugin-react'
+import { globSync } from 'glob'
 import { defineConfig } from 'vite'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const assetsDir = '__bundle'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  appType: 'mpa',
+  root: path.resolve(__dirname, 'src', 'pages'),
+  publicDir: path.resolve(__dirname, 'public'),
+  build: {
+    emptyOutDir: true,
+    outDir: path.resolve(__dirname, 'dist'),
+    assetsDir: assetsDir,
+    rollupOptions: {
+      input: globSync(path.resolve(__dirname, 'src', 'pages', '*.html')),
+      output: {
+        assetFileNames: (_assetInfo) => {
+          return `${assetsDir}/[name]-[hash][extname]`
+        },
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname),
+    },
+  },
   plugins: [react()],
 })
